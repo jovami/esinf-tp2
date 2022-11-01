1fe0a39bdbad332d019965601bf12e1b9ff377c9
Yuhf7 <77704716+Yuhf7@users.noreply.github.com>
2022-11-01 15:53:01 +0000

    feat(KDTree): added interface

---
+++ src/main/java/jovami/trees/KDTree.java
@@ -0,0 +1,12 @@
+package jovami.trees;
+
+import java.awt.geom.Point2D;
+import java.util.Comparator;
+
+/**
+ * KDTree
+ */
+public class KDTree<E extends Comparable<E> & KDInterface<E>> extends BST<E> {
+
+
+}

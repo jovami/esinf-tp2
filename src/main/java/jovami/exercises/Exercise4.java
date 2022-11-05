4734a1f4d2b2d037ae08a1cfe77b9d1c0d0fcb5c
José Diogo Nápoles de Sousa Rente <1211155@isep.ipp.pt>
2022-11-05 16:20:27 +0000

    feat(ex4): simplified the code in class Exercicio4

--- src/main/java/jovami/exercises/Exercise4.java
+++ src/main/java/jovami/exercises/Exercise4.java
@@ -24,26 +24,20 @@ public class Exercise4 implements Runnable {
     public void run() {
         final double x = 41.14961, y = -8.61099; //x=latitude, y=longitude
         final String itemCPC = "01315", elementCode = "5510", year = "2018";
-        var matchingAreas = getAreas(itemCPC, elementCode, year);
+        getAreas(itemCPC, elementCode, year);
+
+        kdTree.nearestNeighbor(x, y);
     }
 
-    public List getAreas(String itemCPC, String elementCode, String yearCode){
-        final var matchingAreas = new ArrayList<Triplet<Area, Double, Double>>();
-        app.getAreaTree().getTree().inOrder().forEach( area -> {
+    public void getAreas(String itemCPC, String elementCode, String yearCode){
+        app.getAreaTree().getTree().inOrder().forEach(area -> {
             if (area.getItemByItemCode(itemCPC) != null){
                 if (area.getItemByItemCode(itemCPC).getElementByElementCode(elementCode) != null){
                     if(area.getItemByItemCode(itemCPC).getElementByElementCode(elementCode).getYearByYearCode(yearCode) != null){
-                        matchingAreas.add(new Triplet<>(area, area.getCoords().getLatitude(), area.getCoords().getLongitude()));
+                        kdTree.insert(area, area.getCoords().getLatitude(), area.getCoords().getLongitude());
                     }
                 }
             }
         });
-        return matchingAreas;
-    }
-
-    public void loadKDTree(List<Triplet<Area, Double, Double>> list){
-        for (var element: list){
-            kdTree.insert(element.first(), element.second(), element.third());
-        }
     }
 }

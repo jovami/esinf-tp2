80a8e94f3b6a3d2d06eff86e5ae35d6b928ccfe7
Diogo Nápoles <93193669+diogonapoles@users.noreply.github.com>
2022-11-06 17:34:35 +0000

    feat(ex4): ex4 to main (#13)
    * feat(ex4): implementation of a method that returns a Triplet with areas that share similar items, elements and years, as long with their respective coordinates
    
    * feat(ex4): simplified the code in class Exercicio4
    
    * feat(ex4): minor optimizations and JavaDoc
    
    * feat(ex4): Implemented Optional variables and optimised the code
    
    * feat(ex4): implementation of a method that returns a Triplet with areas that share similar items, elements and years, as long with their respective coordinates
    
    * feat(ex4): simplified the code in class Exercicio4
    
    * feat(ex4): minor optimizations and JavaDoc
    
    * feat(ex4): Implemented Optional variables and optimised the code
    
    * feat(ex4): Optional elements
    
    * feat(ex4): KD tree tests
    
    * feat(ex4): implementation of a method that returns a Triplet with areas that share similar items, elements and years, as long with their respective coordinates
    
    * feat(ex4): minor optimizations and JavaDoc
    
    * feat(ex4): Implemented Optional variables and optimised the code
    
    * feat(ex4): simplified the code in class Exercicio4
    
    * feat(ex4): minor optimizations and JavaDoc
    
    * feat(ex4): Implemented Optional variables and optimised the code
    
    * feat(ex4): KD tree tests
    
    * feat(ex4): remove imports

--- src/main/java/jovami/exercises/Exercise4.java
+++ src/main/java/jovami/exercises/Exercise4.java
@@ -2,17 +2,56 @@ package jovami.exercises;
 
 import jovami.App;
 
+import jovami.model.Area;
+import jovami.model.Element;
+import jovami.model.Item;
+import jovami.model.Year;
+
+import jovami.trees.KDTree;
+
+import java.util.Optional;
+
 public class Exercise4 implements Runnable {
 
     private final App app;
+    private final KDTree<Area> kdTree ;
+
 
     public Exercise4() {
         app = App.getInstance();
-
+        this.kdTree = new KDTree<>();
     }
 
     @Override
     public void run() {
+        final double x = 41.14961, y = -8.61099; //x=latitude, y=longitude
+        final String itemCode = "156", elementCode = "5419", year = "1965";
+        getAreas(itemCode, elementCode, year);
 
+        Area nearestArea = (Area) kdTree.nearestNeighbor(x, y);
+    }
+
+    public void getAreas(String itemCode, String elementCode, String yearCode){
+        app.getAreaTree().getTree().forEach(area -> {
+            Optional<Item> item = area.getItemByItemCode(itemCode);
+            if(item.isPresent()) {
+                Optional<Element> element = item.get().getElementByElementCode(elementCode);
+                if(element.isPresent()){
+                    Optional<Year> year = element.get().getYearByYearCode(yearCode);
+                    if (year.isPresent())
+                        kdTree.insert(area, area.getCoords().getLatitude(), area.getCoords().getLongitude());
+                }
+            }
+        });
+    }
+
+    public void kdTest(){
+        int[] arrayteste= new int[]{1,2,3,4,5,6,7,8,9};
+        int[] coordsx= new int[]{2,5,10,-3,6,8,20,35,8};
+        int[] coordsy= new int[]{2,5,10,-3,6,8,20,35,8};
+        KDTree<Integer> teste = new KDTree();
+        for (int i = 0; i < arrayteste.length; i++) {
+            teste.insert(arrayteste[i], coordsx[i], coordsy[i]);
+        }
     }
 }

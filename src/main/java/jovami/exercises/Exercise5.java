d1cdb54be135f3ff47af88d5148e2cacb6c079c4
unknown <1210951@isep.ipp.pt>
2022-11-07 13:13:53 +0000

    changed static variable to sum the value with a DoubleAdder

--- src/main/java/jovami/exercises/Exercise5.java
+++ src/main/java/jovami/exercises/Exercise5.java
@@ -1,6 +1,8 @@
 package jovami.exercises;
 
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.DoubleAdder;
 
 import jovami.App;
 import jovami.model.Element;
@@ -24,35 +26,36 @@ public class Exercise5 implements Runnable {
     @Override
     public void run() {
         
-        String itemCode = "252";
+        String itemCode = "1058";
         String elementCode = "5510";
-        String yearCode = "1961";
+        String yearCode = "2018";
         
-        double latitudeInicial = 150;
-        double latitudeFinal = -180;
+        double latitudeInicial = -30;
+        double latitudeFinal = 30;
 
-        double longitudeInicial = 180;
-        double longitudeFinal = -180;
+        double longitudeInicial = 10;
+        double longitudeFinal = 40;
 
         Optional<Item> itemDesired = itemStore.getItemByItemCode(itemCode);
         
         if(!itemDesired.isPresent()){
             return;
         }
-
-        float sumTotal=getRangeSum(elementCode, yearCode, latitudeInicial, latitudeFinal, longitudeInicial, longitudeFinal, itemDesired);
+        double sumTotal=getRangeSum(elementCode, yearCode, latitudeInicial, latitudeFinal, longitudeInicial, longitudeFinal, itemDesired);
         
+
+        System.out.printf("Item Code: %s\nElement Code: %s\nYear Code:%s\n",itemCode,elementCode,yearCode );
+        System.out.printf("Latitude inicial: %f\nLatitude final: %f\n---------------\nLongitude inicial: %f\nLongitude final: %f\n\n",
+            latitudeInicial,latitudeFinal,longitudeInicial,longitudeFinal);
         System.out.println("Soma total de valores dentro da area forncecida :"+sumTotal);
         
     }
 
-    private float getRangeSum(String elementCode, String yearCode, double latitudeInicial, double latitudeFinal,
+    private double getRangeSum(String elementCode, String yearCode, double latitudeInicial, double latitudeFinal,
             double longitudeInicial, double longitudeFinal, Optional<Item> itemDesired) {
 
-        var wrap = new Object(){
-                float sumTotal=0;
-        };
-
+        DoubleAdder sum = new DoubleAdder();
+        
 
         if(longitudeFinal<longitudeInicial){
             double backup = longitudeFinal;
@@ -68,7 +71,6 @@ public class Exercise5 implements Runnable {
 
         areaTree.getKDtree().rangeSearch(latitudeInicial, longitudeInicial, latitudeFinal, longitudeFinal)
             .forEach(area->{
-                
                 Optional<Item> item= area.getTreeItem().find(itemDesired.get());
                 if(!item.isPresent()){
                     return;
@@ -78,7 +80,6 @@ public class Exercise5 implements Runnable {
                 if(!element.isPresent()){
                     return;
                 }
-
                 Optional<Year> year = element.get().getYearByYearCode(yearCode);
                 if(!year.isPresent()){
                     return;
@@ -86,10 +87,9 @@ public class Exercise5 implements Runnable {
                 
                 Optional<Float> value = year.get().getValue().getValue();
                 if(value.isPresent()){
-                    wrap.sumTotal += value.get();
+                    sum.add(value.get());
                 }
         });
-
-        return wrap.sumTotal;
+        return sum.doubleValue();
     }
 }
